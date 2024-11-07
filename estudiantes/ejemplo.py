fc4413364bdd57abaf7a9b58ee20e3be690778c0
KenethUTN <159497299+KenethUTN@users.noreply.github.com>
2024-11-06 21:04:37 -0600

    Update ejemplo.py
    planteos de clase

--- estudiantes/ejemplo.py
+++ estudiantes/ejemplo.py
@@ -1,8 +1,10 @@
 #estudiante
 
 class Estudiante():
-    def __init__(self, nombre):
+    def __init__(self, nombre, edad):
         self.nombre = nombre
+        self.edad = edad
 
     def saludar(self):
         print("Nombre: " + self.nombre)
+        print("Edad de :" self.nombre " : " + self.edad)
