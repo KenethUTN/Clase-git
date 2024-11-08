a6f08e3594e0cb2e71adf299f9bd7f10fcac6ed9
DJAA12 <dayronanchia192003@gmail.com>
2024-11-07 19:42:45 -0600

    Primer ejercicio

---
+++ estudiantes/dayron.py
@@ -0,0 +1,8 @@
+#Dayron
+
+class Dayron():
+    def __init__(self, nombre):
+        self.nombre = nombre
+
+    def saludar(self):
+        print("Nombre: " + self.nombre)
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from estudiantes import ejemplo
+from estudiantes import dayron
 
 if __name__ == '__main__':
     estudiante = ejemplo.Estudiante("Ejemplo")
     estudiante.saludar()
     
-    
+    DAYRON = dayron.Dayron("Dayron")
+    DAYRON.saludar()
