c876b08456767f4d8e921df98c9e5c4d9f7ff6c2
XxKeyDarkxX <kescalantem@est.utn.ac.cr>
2024-11-07 19:46:02 -0600

    Primer ejercicio

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from estudiantes import ejemplo
+from estudiantes import Keylor
 
 if __name__ == '__main__':
     estudiante = ejemplo.Estudiante("Ejemplo")
     estudiante.saludar()
     
-    
+    KEYLOR = Keylor.Keylor("keylor")
+    KEYLOR.saludar()
