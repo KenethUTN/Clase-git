8b83b7098ffd0bcbe7e17617d43cea7c5b521007
YohelRojas <yohelrg9814@gmail.com>
2024-11-07 19:42:43 -0600

    Clase Profe Keneth

---
+++ estudiantes/yohel.py
@@ -0,0 +1,8 @@
+#Yohel
+
+class Yohel():
+    def __init__(self, nombre):
+        self.nombre = nombre
+
+    def saludar(self):
+        print("Nombre: " + self.nombre)
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from estudiantes import ejemplo
+from estudiantes import yohel
 
 if __name__ == '__main__':
     estudiante = ejemplo.Estudiante("Ejemplo")
     estudiante.saludar()
     
+    YOHEL = yohel.Yohel("Yohel")
+    YOHEL.saludar()
     
