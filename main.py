5e8b3eced654cb0f8b87eff9e4c5e058486146cf
dorian2909 <doumanaca@est.utn.ac.cr>
2024-11-07 19:44:35 -0600

    commit de dorian

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from estudiantes import ejemplo
+from estudiantes import dorian
 
 if __name__ == '__main__':
     estudiante = ejemplo.Estudiante("Ejemplo")
     estudiante.saludar()
-    
+
+    DORIAN = dorian.Dorian("dorian")
+    DORIAN.saludar()
+
     
