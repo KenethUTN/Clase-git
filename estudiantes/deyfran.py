60ac69b94ec8329f0ed43051d1bb717ad342bf17
Deyfran58 <dearagonca@est.ac.cr>
2024-11-07 19:44:24 -0600

    Primer Ejercicio

---
+++ estudiantes/deyfran.py
@@ -0,0 +1,8 @@
+#deyfran
+
+class deyfran():
+    def __init__(self, nombre):
+        self.nombre = nombre
+
+    def saludar(self):
+        print("Nombre: " + self.nombre)
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from estudiantes import ejemplo
+from estudiantes import deyfran
 
 if __name__ == '__main__':
     estudiante = ejemplo.Estudiante("Ejemplo")
     estudiante.saludar()
-    
+
+
+    deyfran = deyfran.deyfran("deyfran")
+    deyfran.saludar()
     
