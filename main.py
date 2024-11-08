eed5f1260735a3510f8e5e2f5a3f6aa9d9f85137
DianaSosa <disosade@est.utn.ac.cr>
2024-11-07 19:40:26 -0600

    Primer ejercicio

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from estudiantes import ejemplo
+from estudiantes import diana
 
 if __name__ == '__main__':
     estudiante = ejemplo.Estudiante("Ejemplo")
     estudiante.saludar()
+
+    DIANA = diana.diana("diana")
+    DIANA.saludar()
+    
     
     
