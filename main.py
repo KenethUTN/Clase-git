4efd67ae5ab83adda044c31fff90c4684f30599c
unknown <kegonzalezca@est.utn.ac.cr>
2024-11-07 20:07:47 -0600

    modificaciones

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from estudiantes import ejemplo
 from estudiantes import keneth
 
 if __name__ == '__main__':
-    estudiante = ejemplo.Estudiante("Ejemplo")
+    estudiante = ejemplo.Estudiante("Ejemplo", 00)
     estudiante.saludar()
 
-    KENETH = keneth.Keneth("Keneth")
+    KENETH = keneth.Keneth("Keneth", 21)
     KENETH.saludar()
 
 
