a7cf8efbe059542f9924316c9acaf777fc4be30a
Jonathan Zuniga <jozunigabu@est.utn.ac.cr>
2024-11-07 19:42:46 -0600

    primer ejercicio

---
+++ estudiantes/jonathan.py
@@ -0,0 +1,8 @@
+#jonathan
+
+class Jonathan():
+    def __init__(self, nombre):
+        self.nombre = nombre
+
+    def saludar(self):
+        print("Nombre: " + self.nombre)
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from estudiantes import ejemplo
+from estudiantes import jonathan
 
 if __name__ == '__main__':
     estudiante = ejemplo.Estudiante("Ejemplo")
     estudiante.saludar()
     
-    
+    JONATHAN = jonathan.Jonathan("Jonathan")
+    JONATHAN.saludar()
