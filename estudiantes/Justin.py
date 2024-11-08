91c81a24ad9501e5efba0d8829d9cd51961e6037
Justin <justinvargas4299@gmail.com>
2024-11-07 19:42:41 -0600

    Primer ejercicio

---
+++ estudiantes/Justin.py
@@ -0,0 +1,8 @@
+#Justin
+
+class Justin():
+    def __init__(self, nombre):
+        self.nombre = nombre
+
+    def saludar(self):
+        print("Nombre: " + self.nombre)
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from estudiantes import ejemplo
+from estudiantes import Justin
 
 if __name__ == '__main__':
     estudiante = ejemplo.Estudiante("Ejemplo")
     estudiante.saludar()
+
+    JUSTIN = Justin.Justin("Justin")
+    JUSTIN.saludar()
     
     
