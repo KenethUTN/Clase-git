0ea1c66942ae04ad304069699f37076be58e72f6
ValeryRJ123 <valeryrodriguezjimenez65@gmail.com>
2024-11-07 19:42:59 -0600

    Primer ejercicio

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from estudiantes import ejemplo
+from estudiantes import valery
 
 if __name__ == '__main__':
     estudiante = ejemplo.Estudiante("Ejemplo")
     estudiante.saludar()
     
+    VALERY = valery.Valery("Valery")
+    VALERY.saludar()
     
