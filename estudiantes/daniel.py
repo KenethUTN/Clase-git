20477b208e8186d9f30bc7844f91d2166a7b3a90
Anisan07 <mejiadano07@gmail.com>
2024-11-07 19:40:36 -0600

    Commit de daniel v1.0

---
+++ estudiantes/daniel.py
@@ -0,0 +1,8 @@
+#Daniel
+
+class Daniel():
+    def __init__(self, nombre):
+        self.nombre = nombre
+
+    def saludar(self):
+        print("Nombre: " + self.nombre)
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from estudiantes import ejemplo
+from estudiantes import daniel
 
 if __name__ == '__main__':
     estudiante = ejemplo.Estudiante("Ejemplo")
     estudiante.saludar()
     
-    
+    DANIEL = daniel.Daniel("Daniel")
+    DANIEL.saludar()
