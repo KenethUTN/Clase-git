8a06a32e684bbffa8d65a0e8e4e38537600cddc2
unknown <kegonzalezca@est.utn.ac.cr>
2024-11-07 19:41:29 -0600

    Primer ejercicio

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from estudiantes import ejemplo
+from estudiantes import keneth
 
 if __name__ == '__main__':
     estudiante = ejemplo.Estudiante("Ejemplo")
     estudiante.saludar()
+
+    KENETH = keneth.Keneth("Keneth")
+    KENETH.saludar()
+
+
+
     
     
